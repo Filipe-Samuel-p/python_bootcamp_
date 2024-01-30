6aa74f56de390d8ba3c5067140446ac10c249d46
Filipe Samuel <felipepires.p125@gmail.com>
2024-01-29 23:51:00 -0300

    Adicionando o jogo de leilao

---
+++ secret_auction/main.py
@@ -0,0 +1,23 @@
+import os
+from logo import logo
+
+dicionario ={}
+parametro = 0
+print(logo)
+while True:
+   nome_do_usuario = input("Qual o seu nome? ")
+   valor_pago = float(input("De quanto será o lance: "))
+   dicionario[nome_do_usuario] = valor_pago
+   if valor_pago >= parametro:
+      parametro = valor_pago
+      nome_de_quem_tem_o_maior_valor = nome_do_usuario
+
+   verificacao = input("Existem mais pessoas para jogar? (sim) ou (nao): ")
+   
+   if verificacao.lower() == "sim":
+      os.system("clear")  #limpa o terminal 
+      continue
+   else:
+      break
+print(f"o maior valor é de { nome_de_quem_tem_o_maior_valor} e a quantia foi de {parametro}")
+
