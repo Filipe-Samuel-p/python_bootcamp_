ebc16b362d95a33d4b6e5c2bc2cd9208dea32d1d
Filipe Samuel <felipepires.p125@gmail.com>
2024-01-31 14:03:19 -0300

    jogo finalizado

--- higher lower game/main.py
+++ higher lower game/main.py
@@ -1,18 +1,35 @@
 from dados import data
 from random import randint
 import logo
+import os
+
 
 indice = randint(0,49)
 pontos = 0
 while True:
-    print(f"Compare A: {data[indice]["name"]},{data[indice]['description']}, from {data[indice]["country"]}")
+    os.system("clear")
+    print(logo.logo)
+    print(f"Sua potuaçao é {pontos}\n")
+    print(f"Personalidade A: {data[indice]["name"]}, {data[indice]['description']}, from {data[indice]["country"]}")
     print(logo.vs)
     indice2 = randint(0,49)
-    print(f"Compare B: {data[indice2]["name"]},{data[indice2]['description']}, from {data[indice2]["country"]}")
-    if data[indice]["follower_count"] > data[indice2]["follower_count"]:
-        pontos += 1
+    print(f"Personalidade B: {data[indice2]["name"]}, {data[indice2]['description']}, from {data[indice2]["country"]}\n")
+    decisao = input("Voce escolhe 'A'ou 'B'? : ")
+    decisao_e_verificacao_A = decisao.lower() == "a" and data[indice]["follower_count"] > data[indice2]["follower_count"]
+    decisao_e_verificacao_B = decisao.lower() == "b" and data[indice]["follower_count"] < data[indice2]["follower_count"]
+    if decisao_e_verificacao_A or decisao_e_verificacao_B:
+       pontos += 1
+       if decisao_e_verificacao_A:
+        continue
+       else:
+          indice = indice2 
     else:
-        ...
+        os.system("clear")
+        print('Voce perdeu!!!')
+        print(f"Sua potuaçao é {pontos}")
+        break
+
+
 
  
         
