9c80581e1aa19c0b748158f9527498a82201b102
Filipe Samuel <felipepires.p125@gmail.com>
2024-01-30 23:26:11 -0300

    Adicionando o jogo de comparaçao entre celebridades

---
+++ higher lower game/main.py
@@ -0,0 +1,14 @@
+from dados import data
+from random import randint
+import logo
+
+indice = randint(0,49)
+pontos = 0
+while True:
+    print(f"Compare A: {data[indice]["name"]},{data[indice]['description']}, from {data[indice]["country"]}")
+    print(logo.vs)
+    indice2 = randint(0,49)
+    print(f"Compare B: {data[indice2]["name"]},{data[indice2]['description']}, from {data[indice2]["country"]}")
+    if data[indice]["follower_count"] > data[indice2]["follower_count"]:
+        pontos += 1
+        
