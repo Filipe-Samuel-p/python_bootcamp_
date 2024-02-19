efa92572dbbdfd85bf63e3351b8da634f5b0710c
Filipe Samuel <felipepires.p125@gmail.com>
2024-02-18 23:47:07 -0300

    alterando

---
+++ modulo Turtle/main.py
@@ -0,0 +1,26 @@
+from turtle import *
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+
+""" for i in range(4):
+    timmy_the_turtle.forward(100) #forward é movimentar a tartaruga para a frente. e passa como argumento a distancia
+    timmy_the_turtle.right(90)"""
+for i in range(10):
+ print(" ", end = " ")
+ timmy_the_turtle.forward(10)
+
+
+
+
+
+
+
+
+exitonclick()
+
+
+
+
+
+
