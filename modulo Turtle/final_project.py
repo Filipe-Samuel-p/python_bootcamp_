ecf822d1176841668fde24adee1a2bdc0d890fe2
Filipe Samuel <felipepires.p125@gmail.com>
2024-02-28 23:58:34 -0300

    Adicionando onkey( )

--- modulo Turtle/final_project.py
+++ modulo Turtle/final_project.py
@@ -1,16 +1,32 @@
 from turtle import *
+import os
 
-def movimento():
+def movimento_W():
     tim.forward(10)
 
+def movimento_para_tras():
+    tim.backward(10)
+
+def movimento_direita():
+    tim.right(30)
+
+def movimento_esquerda():
+    tim.left(30)
+
+def limpeza():
+    os.system("clear")
+    tim.home()
 
 
 tim = Turtle()
 
 listen() # funcao que faz a tela "ouvir" um comando e executar algo
-onkey(key="space", fun= movimento) # funcao que quando a tecla "espaco"(poderia ser outra) for acionada, uma funcao ira ser usada
+onkey(key="w", fun= movimento_W) # funcao que quando a tecla "espaco"(poderia ser outra) for acionada, uma funcao ira ser usada
                                    # no caso, essa funcao usada é a "movimento", que cuida do movimento da turtle
-
+onkey(key="s", fun= movimento_para_tras)
+onkey(key="d", fun= movimento_direita)
+onkey(key="a", fun= movimento_esquerda)
+onkey(key="c", fun= limpeza)
 
 
 
