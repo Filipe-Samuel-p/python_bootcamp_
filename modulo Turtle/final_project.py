aa199cf25b26705ce71d65033b7f390ce926280e
Filipe Samuel <felipepires.p125@gmail.com>
2024-02-28 23:41:59 -0300

    desenvolvendo a GUI

--- modulo Turtle/final_project.py
+++ modulo Turtle/final_project.py
@@ -1,2 +1,24 @@
 from turtle import *
-import random
+
+def movimento():
+    tim.forward(10)
+
+
+
+tim = Turtle()
+
+listen() # funcao que faz a tela "ouvir" um comando e executar algo
+onkey(key="space", fun= movimento) # funcao que quando a tecla "espaco"(poderia ser outra) for acionada, uma funcao ira ser usada
+                                   # no caso, essa funcao usada é a "movimento", que cuida do movimento da turtle
+
+
+
+
+
+
+
+
+
+
+
+exitonclick()
