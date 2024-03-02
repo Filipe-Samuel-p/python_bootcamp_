60dc7fc616727505756a28fae1754d7ca33b3ae5
Filipe Samuel <felipepires.p125@gmail.com>
2024-03-02 01:44:29 -0300

    snake game project

--- snake game/main.py
+++ snake game/main.py
@@ -1,34 +1,31 @@
 from turtle import *
 import random
 
-def movimento_w():
-    snake.right(90)
-    snake.forward(10)
+# TELA (screen)
+title("SNAKE GAME")
+setup(width= 700, height= 600)
+bgcolor("black")
 
-def movimento_s():
-    snake.right(90)
-    snake.backward(10)
-def movimento_():
-    snake.forward(10)
-def movimento_w():
-    snake.forward(10)
 
 posicao_de_x = random.randint(-330,331)
 posicao_de_y = random.randint(-280,280)
 
 
-title("SNAKE GAME")
-setup(width= 700, height= 600)
-bolinha = Turtle(shape="circle")
-bolinha.speed(-1)
-bolinha.color("white")
-snake = Turtle(shape="square")
-snake.color("white")
-bgcolor("black")
+x=0
+y=0
+cobra = []
+jogo_funcionando = True
+for i in range(4):
+    snake = Turtle(shape="square")
+    snake.color("white")
+    snake.penup()
+    snake.goto(x=x,y=y)
+    x -= 10
+    cobra.append(snake)
 
-listen()
-onkey(key= "w", fun=movimento_w)
-onkey(key= "s", fun=movimento_s)
+
+while jogo_funcionando:
+    ...
 
 
 
