b26a869cb69b6ee3694be7c3ef19cb2f3299389c
Filipe Samuel <felipepires.p125@gmail.com>
2024-03-12 23:49:25 -0300

    ...

--- snake game/comida.py
+++ snake game/comida.py
@@ -1,6 +1,7 @@
 from turtle import *
 import random
 
+
 class Food(Turtle): #usando herança
     def __init__(self):
         super().__init__()
@@ -10,7 +11,7 @@ class Food(Turtle): #usando herança
         self.color("red")
         self.speed("fastest")
         self.nova_posicao()
-
+ 
     def nova_posicao(self):
        posicao_x = random.randint(-330,331)
        posicao_y = random.randint(-280,281)
