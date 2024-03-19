cba97afd1c8852403884a752cc47dfccfc967d26
Filipe Samuel <felipepires.p125@gmail.com>
2024-03-19 11:38:47 -0300

    tkinter introducion

--- Tkinter GUI/pratice1.py
+++ Tkinter GUI/pratice1.py
@@ -1,7 +1,30 @@
-import tkinter 
+from tkinter import *
 
-janela = tkinter.Tk()
+# janela 
+janela = Tk()
 janela.title("My first GUI program")
+janela.minsize(width=500,height=500)
+
+#rótulo
+
+rotulo = Label(text="Testando Rótulos")
+rotulo.pack()
+
+#botão
+x = 0
+def botao_clicado():
+   x += 1
+   str(x)
+   rotulo.config(text=x)
+   
+
+botao = Button(text="Click-me",command=botao_clicado)
+botao.pack() #metodo para mostrar um objeto na tela
+
+#entrada 
+
+entrada = Entry()
+entrada.pack()
 
 
 
